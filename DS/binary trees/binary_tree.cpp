9044135b4260eb45e39eff74bff7948763149568
SaiSri Angajala <63279776+SaiSri44@users.noreply.github.com>
2021-10-06 20:44:53 +0530

    made some modifications in binary tree construction file

--- DS/binary trees/binary_tree.cpp
+++ DS/binary trees/binary_tree.cpp
@@ -3,25 +3,29 @@ using namespace std;
 
 struct node
 {
-    struct node *left;
+    node *left, *right;
     int data;
-    struct node *right;
+    // node *right;
+    node(int x)
+    {
+        data = x;
+        left = right = NULL;
+
+        // making left and right pointers poiting to null,this is very important because if we not made them to null,they will store some garbage values which points to another locations,this leads to wrong output in the traversals
+    }
 };
-struct node *root = NULL;
+node *root = NULL;
 void create()
 {
-    deque<struct node *> q;
+    deque<node *> q;
     // deque for storing the address of the nodes
     int x;
-    struct node *temp, *p;
-    // p points to the current node,and temp is used for creating the left and right childs of the current node
-    root = new node;
-    // first create the root node
-    root->left = root->right = NULL;
-    // make left and right child of the root to NULL
+    node *p;
+    // p points to the current node
     cout << "Enter the data of the root node " << endl;
-    // reading the data of the root node
-    cin >> root->data;
+    cin >> x;
+    // creating a new node with value x
+    root = new node(x);
     q.push_back(root);
     // pushing the root node into the queue for creating the right child and left child for the root node
     while (!q.empty())
@@ -36,65 +40,45 @@ void create()
         // if the entered value is -1,that mean there is no child for that node,if not there is child
         if (x != -1)
         {
-            temp = new node;
-            // creating the new node for the left child
-            temp->data = x;
-            temp->left = temp->right = NULL;
-            // Assinging the value
-            p->left = temp;
+            p->left = new node(x);
             // making the current nodes left pointing to newly created node,which is left child of current node
-            q.push_back(temp);
+            q.push_back(p->left);
             // pushing the left child into queue,because we also want to create childs of this node too
         }
         cout << "Enter the right child of the node : " << p->data << endl;
         cin >> x;
         if (x != -1)
         {
-            temp = new node;
-            temp->data = x;
-            temp->left = temp->right = NULL;
-            p->right = temp;
+            p->right = new node(x);
             // making the current node right pointing to newly created node,which is right child of current node
-            q.push_back(temp);
+            q.push_back(p->right);
             // pushing the right child into queue,because we also want to create childs of this node too
         }
     }
     // in this approach we are creating the tree level by level,that mean first we finish the level and after that we go for another level
 }
 
-struct node *rcreate()
+node *rcreate()
 {
     // this is recursive approach for creating the binary tree
     int x;
     // for storing the data of the newly created node
-    static int i = 0;
-    // for identification of the root node
-    if (i == 0)
-    {
-        cout << "Enter the root node value " << endl;
-        //   if i==0 then it is root node,else it is internal node
-        i++;
-    }
+    cout << " Enter the data " << endl;
     cin >> x;
     // reading the input from stdin
     if (x == -1)
         // base case,if there is no child either right or left return NULL
         return NULL;
     // if x is not -1,then create a new node
-    struct node *new_node = new node;
-    // assinging the data of the node
-    new_node->data = x;
-    // making left and right pointers poiting to null,this is very important because if we not made them to null,they will store some garbage values which points to another locations,this leads to wrong output in the traversals
-    new_node->right = new_node->left = NULL;
-
-    cout << "Enter the left child of the node  " << new_node->data << endl;
-    // getting the data of the left child for the current node
-    new_node->left = rcreate();
-    // getting the data of the right child of the current node
-    cout << "Enter the right child of the node  " << new_node->data << endl;
-    new_node->right = rcreate();
+    node *curr = new node(x);
+    cout << "Enter the left child of the node  " << curr->data << endl;
+    // getting the data of the left child of the current node
+    curr->left = rcreate();
+    cout << "Enter the right child of the node " << curr->data << endl;
+    // getting the data of the right child for the current node
+    curr->right = rcreate();
     // returning the  current node
-    return new_node;
+    return curr;
 
     // In this approach we are not consturcting the binary tree level by level, first we are creating the left childs of the nodes,after that in reverse traverse we are creating the right childs
 
@@ -106,7 +90,23 @@ struct node *rcreate()
     // is created
 }
 
-struct node *rcreate2()
+// construct binary tree from the array of elements
+// array contains the level order traversal of the elements
+
+node *constructtree(vector<int> &arr, int i, int n)
+{
+    if (i >= n or arr[i] == -1)
+        return NULL;
+
+    node *new_node;
+    new_node = new node(arr[i]);
+    new_node->left = constructtree(arr, 2 * i + 1, n);
+    new_node->right = constructtree(arr, 2 * i + 2, n);
+
+    return new_node;
+}
+
+node *rcreate2()
 {
     int x;
     static int i = 0;
@@ -118,9 +118,7 @@ struct node *rcreate2()
     cin >> x;
     if (x == -1)
         return NULL;
-    struct node *new_node = new node;
-    new_node->data = x;
-    new_node->right = new_node->left = NULL;
+    struct node *new_node = new node(x);
     cout << "Enter the right child of the node  " << new_node->data << endl;
     new_node->right = rcreate2();
     cout << "Enter the left child of the node  " << new_node->data << endl;
@@ -128,32 +126,32 @@ struct node *rcreate2()
     return new_node;
 
     // In this approach we are not consturcting the binary tree level by level, first we are creating the right childs of the nodes,after that in reverse traverse we are creating the left childs
-}
-void preorder(struct node *temp)
+} 
+void preorder(node *temp)
 {
-    if (temp == NULL)
+    if (!temp)
         return;
     cout << temp->data << " ";
     preorder(temp->left);
     preorder(temp->right);
 }
-void inorder(struct node *temp)
+void inorder(node *temp)
 {
-    if (temp == NULL)
+    if (!temp)
         return;
     inorder(temp->left);
     cout << temp->data << " ";
     inorder(temp->right);
 }
-void postorder(struct node *temp)
+void postorder(node *temp)
 {
-    if (temp == NULL)
+    if (!temp)
         return;
     postorder(temp->left);
     postorder(temp->right);
     cout << temp->data << " ";
 }
-void iterative_preorder(struct node *temp = root)
+void iterative_preorder(node *temp = root)
 {
     // iterative approach for preorder
     stack<struct node *> stack;
@@ -196,7 +194,7 @@ void iterative_inorder(struct node *temp = root)
 
 int main()
 {
-    root = rcreate(); //for recursive construction of the tree
+     root = rcreate(); //for recursive construction of the tree
     // create() //for iterative construction of tree
     cout << "preorder traversal of the binary tree is : " << endl;
     preorder(root);
@@ -212,4 +210,4 @@ int main()
     // cout << endl;
     cout << "postorder traversal of the binary tree is : " << endl;
     postorder(root);
-}
+} 
--- DS/binary trees/checking_bst.cpp
+++ DS/binary trees/checking_bst.cpp
@@ -68,46 +68,56 @@ bool checksorted()
 
 // ------------------METHOD2------------------
 // In bst every subtree is also binary search tree, so maximum value in left subtree is less than current node data and minimum value in right subtree is greater than the current node data, if this condition valid for every node then it is bst
+
+// code for finding the maximum element in left subtree
 int maxleft(node *root, node *temp)
 {
     int lmax, rmax, maxval;
     if (!temp)
         return INT_MIN;
-    lmax = maxleft(root,temp->left);
-    rmax = maxleft(root,temp->right);
+    lmax = maxleft(root, temp->left);
+    rmax = maxleft(root, temp->right);
     maxval = max({lmax, rmax, temp->data});
     if (temp == root)
         return lmax;
     else
         return maxval;
+    // if there is no leftsubtree it returns INT_MIN
 }
+// code for finding minimum element in right subtree
 int minright(node *root, node *temp)
 {
     int lmin, rmin, minval;
     if (!temp)
         return INT_MAX;
-    int lmin = minright(root,temp->left);
-    int rmin = minright(root,temp->right);
+    int lmin = minright(root, temp->left);
+    int rmin = minright(root, temp->right);
     minval = min({lmin, rmin, temp->data});
     if (temp == root)
         return rmin;
     else
-       return minval;
-} 
+        return minval;
+    // if there is no rightsubtree it returns INT_MAX
+}
 int checkbst(node *root)
 {
-    
-    if(!root)
+    // if the node is null then return 1
+    if (!root)
         return 1;
-    if(root->left!=NULL and maxleft(root) > root->data)
+    // if there is left subtree and max in left subtree is greater than the current node data then return 0
+    if (root->left != NULL and maxleft(root) > root->data)
         return 0;
-    if(root->right !=NULL and minright(root) < root->data)
+    // if there is right subtree and minimum in right subtree is less than the current node data then return 0
+    if (root->right != NULL and minright(root) < root->data)
         return 0;
-    if(!checkbst(root->left) or !checkbst(root->right))
+    // recursively check for false condition in left and right subtrees
+    if (!checkbst(root->left) or !checkbst(root->right))
         return 0;
+    // if all the above conditions fails then the tree is binary search tree so return 1
     return 1;
-}  
- 
+    // here we are checking from root node onwards
+} 
+
 // --------METHOD2-------------------
 int main()
 
